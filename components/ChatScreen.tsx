ee18aeaa86ea1b59250c71a58a1a85338b24cdbf
Xiao Wen Wu <xiaokilledwu@gmail.com>
2023-08-04 23:15:50 -0400

    Added text

--- components/ChatScreen.tsx
+++ components/ChatScreen.tsx
@@ -4,6 +4,10 @@ import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
 const ChatScreen = ({navigation}) => {
   return (
     <View className="flex flex-col h-full justify-end">
+      <Text>
+        Add person's name here when mapping: Sample, map the chat from the
+        database here
+      </Text>
       <TextInput
         className="bg-white border-2 border-black h-10"
         placeholder="Type a message here..."
