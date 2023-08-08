2af55582c0cea00f1aa371abc87c8045c2ea0d14
Xiao Wen Wu <xiaokilledwu@gmail.com>
2023-08-08 01:31:02 -0400

    Change message sent to db format

--- components/ChatScreen.tsx
+++ components/ChatScreen.tsx
@@ -5,17 +5,20 @@ import database from '@react-native-firebase/database';
 const ChatScreen = ({navigation}) => {
   let [message, setMessage] = React.useState('');
   const [messageArray, setMessageArray] = React.useState([]);
-  let addChat = (message: string) => {
+
+  let addChat = (message: object) => {
     database().ref('/messages').push({
-      name: message,
+      name: 'User',
+      m: message,
     });
   };
   let messageRef = database().ref('/messages');
+
   React.useEffect(() => {
     messageRef.on('value', snapshot => {
       let data = snapshot.val();
-      const items = Object.values(data);
-      setMessageArray(items);
+      const chat = Object.values(data);
+      setMessageArray(chat);
     });
   }, []);
 
@@ -24,21 +27,21 @@ const ChatScreen = ({navigation}) => {
   };
   return (
     <View className="flex flex-col h-full p-2 gap-2 justify-end">
-      <Text>
-        Add person's name here when mapping: Sample, map the chat from the
-        database here
-      </Text>
-      {messageArray.length > 0 ? (
-        messageArray.map(mess => {
-          return (
-            <View>
-              <Text>{mess.name}</Text>
-            </View>
-          );
-        })
-      ) : (
-        <Text>No items</Text>
-      )}
+      <View className="flex flex-col-reverse">
+        {messageArray.length > 0 ? (
+          messageArray.map((mess, index) => {
+            return (
+              <View key={index}>
+                <Text>
+                  {mess.name} : {mess.m}
+                </Text>
+              </View>
+            );
+          })
+        ) : (
+          <Text>Start a chat!</Text>
+        )}
+      </View>
       <TextInput
         className="bg-white border-2 border-black h-10"
         onChangeText={setMessage}
