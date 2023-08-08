5d707c47a9ed594eb5299f6088ebfa3c33ec40dd
Xiao Wen Wu <xiaokilledwu@gmail.com>
2023-08-08 01:03:35 -0400

    Retrieve messages from db

--- components/ChatScreen.tsx
+++ components/ChatScreen.tsx
@@ -4,11 +4,20 @@ import database from '@react-native-firebase/database';
 
 const ChatScreen = ({navigation}) => {
   let [message, setMessage] = React.useState('');
+  const [messageArray, setMessageArray] = React.useState([]);
   let addChat = (message: string) => {
     database().ref('/messages').push({
       name: message,
     });
   };
+  let messageRef = database().ref('/messages');
+  React.useEffect(() => {
+    messageRef.on('value', snapshot => {
+      let data = snapshot.val();
+      const items = Object.values(data);
+      setMessageArray(items);
+    });
+  }, []);
 
   let onSubmitClick = () => {
     addChat(message);
@@ -19,6 +28,17 @@ const ChatScreen = ({navigation}) => {
         Add person's name here when mapping: Sample, map the chat from the
         database here
       </Text>
+      {messageArray.length > 0 ? (
+        messageArray.map(mess => {
+          return (
+            <View>
+              <Text>{mess.name}</Text>
+            </View>
+          );
+        })
+      ) : (
+        <Text>No items</Text>
+      )}
       <TextInput
         className="bg-white border-2 border-black h-10"
         onChangeText={setMessage}
