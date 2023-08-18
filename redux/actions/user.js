e9f016755c9393116c9f96e07315d73992877666
Xiao Wen Wu <xiaokilledwu@gmail.com>
2023-08-18 18:17:14 -0400

    Set up redux

---
+++ redux/actions/user.js
@@ -0,0 +1,10 @@
+//Action Creator
+const setUser = user => {
+  return {
+    type: 'LOGIN',
+    payload: {
+      username: user.name,
+      password: user.password,
+    },
+  };
+};
---
+++ redux/reducers/userReducer.js
@@ -0,0 +1,16 @@
+//Reducer
+const initialState = {
+  username: '',
+  password: '',
+};
+const loggeduser = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        username: action.payload.username,
+        password: action.payload.password,
+      };
+    default:
+      return state;
+  }
+};
